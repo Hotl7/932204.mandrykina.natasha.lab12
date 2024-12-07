0235d8981f0800691ca148e9acacb70ecd8ceb30
hotl7 <66117178+Hotl7@users.noreply.github.com>
2024-12-07 22:38:02 +0700

    first commit

---
+++ lab12/Models/FormData.cs
@@ -0,0 +1,9 @@
+namespace lab12.Models
+{
+    public class FormData
+    {
+        public int firstValue { get; set; }
+        public int secondValue { get; set; }
+        public string operation { get; set; }
+    }
+}
